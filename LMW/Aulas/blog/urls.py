6be1ba387cf140428ad2d6e438878aa9af521a8a
chevitaresebruno <chevitarese.bruno@gmail.com>
2024-11-05 11:46:33 -0300

    Adicionando arquivos certos de LMW

--- LMW/Aulas/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('autor/<int:pk>/', views.author_projects, name='author_projects'),
-]
