7cb4c508bb3b73ef667aff29d20cbbcd3cbefa9a
chevitaresebruno <chevitarese.bruno@gmail.com>
2025-02-14 14:34:58 -0300

    Corrigindo a cagada no trabalho

--- POO/trabalho/parte_3/src/views/user_case/UCGenericCRUD.java
+++ POO/trabalho/parte_3/src/views/user_case/UCGenericCRUD.java
@@ -5,14 +5,14 @@ import database.services.AbstractService;
 import models.BaseModel;
 
 @SuppressWarnings("rawtypes")
-public interface UCGenericCRUD<Model extends BaseModel>
+public interface UCGenericCRUD<Model extends BaseModel, ModelFilter extends BaseFilter<Model>, ModelService extends AbstractService<Model, ModelFilter>>
 {
     public boolean addModel(AbstractService as, Model m);
     public boolean updateModel(AbstractService as, Model m, int where);
-    public boolean updateModel(AbstractService as, Model m, BaseFilter<Model> where);
+    public boolean updateModel(AbstractService as, Model m, ModelFilter where);
     public boolean deleteModel(AbstractService as, int where); 
-    public boolean deleteModel(AbstractService as, BaseFilter<Model> where);
+    public boolean deleteModel(AbstractService as, ModelFilter where);
     public boolean getModel(AbstractService as, int where); 
-    public boolean getModel(AbstractService as, BaseFilter<Model> where);
+    public boolean getModel(AbstractService as, ModelFilter where);
     public boolean listModel(AbstractService as); 
 }
